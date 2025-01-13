cebfcfac7ce3d1a946a485c647b87c822fce1e13
shaheen <aiexpert2025@gmail.com>
2025-01-13 14:12:01 +0300

    add basic form structure for receipts

--- lib/src/features/transactions/view/receipt_form.dart
+++ lib/src/features/transactions/view/receipt_form.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tablets/generated/l10n.dart';
+import 'package:tablets/src/common/forms/date_picker.dart';
 import 'package:tablets/src/common/values/constants.dart';
 import 'package:tablets/src/common/functions/debug_print.dart';
 import 'package:tablets/src/common/forms/drop_down_with_search.dart';
 import 'package:tablets/src/common/forms/edit_box.dart';
 import 'package:tablets/src/common/values/gaps.dart';
+import 'package:tablets/src/common/widgets/custom_icons.dart';
 import 'package:tablets/src/common/widgets/main_frame.dart';
 import 'package:tablets/src/features/transactions/controllers/customer_db_cache_provider.dart';
 import 'package:tablets/src/features/transactions/model/transaction.dart';
@@ -20,13 +22,24 @@ class ReceiptForm extends ConsumerWidget {
         child: Center(
             child: Container(
                 padding: const EdgeInsets.all(12.0),
-                width: 300,
+                width: 500,
                 child: Column(
                   children: [
                     _buildScreenTitle(context, ref),
-                    _buildCustomerNameSelection(context, ref),
                     VerticalGap.xl,
-                    _buildCustomerReceiptAmount(context, ref)
+                    _buildNameSelection(context, ref),
+                    VerticalGap.xl,
+                    _buildReceiptNumber(context, ref),
+                    VerticalGap.xl,
+                    _buildDate(context, ref),
+                    VerticalGap.xl,
+                    _buildReceivedAmount(context, ref),
+                    VerticalGap.xl,
+                    _buildDiscountAmount(context, ref),
+                    VerticalGap.xl,
+                    _buildReceiptTotalAmount(context, ref),
+                    const Spacer(),
+                    _buildButtons(context, ref),
                   ],
                 ))));
   }
@@ -40,7 +53,7 @@ class ReceiptForm extends ConsumerWidget {
         ));
   }
 
-  Widget _buildCustomerNameSelection(BuildContext context, WidgetRef ref) {
+  Widget _buildNameSelection(BuildContext context, WidgetRef ref) {
     final salesmanCustomersDb = ref.read(salesmanCustomerDbCacheProvider.notifier);
 
     return DropDownWithSearch(
@@ -50,19 +63,88 @@ class ReceiptForm extends ConsumerWidget {
     );
   }
 
-  Widget _buildCustomerReceiptAmount(BuildContext context, WidgetRef ref) {
+  Widget _buildReceivedAmount(BuildContext context, WidgetRef ref) {
     return FormInputField(
         onChangedFn: (value) {
           tempPrint(value);
         },
         dataType: FieldDataType.num,
-        name: 'amount');
+        name: 'subtotal');
+  }
+
+  Widget _buildDiscountAmount(BuildContext context, WidgetRef ref) {
+    return FormInputField(
+        onChangedFn: (value) {
+          tempPrint(value);
+        },
+        dataType: FieldDataType.num,
+        name: 'discount');
+  }
+
+  Widget _buildReceiptNumber(BuildContext context, WidgetRef ref) {
+    return FormInputField(
+        onChangedFn: (value) {
+          tempPrint(value);
+        },
+        dataType: FieldDataType.num,
+        name: 'number');
+  }
+
+  Widget _buildReceiptTotalAmount(BuildContext context, WidgetRef ref) {
+    return FormInputField(
+        onChangedFn: (value) {
+          tempPrint(value);
+        },
+        dataType: FieldDataType.num,
+        isReadOnly: true,
+        name: 'total');
+  }
+
+  Widget _buildDate(BuildContext context, WidgetRef ref) {
+    return FormDatePickerField(
+        onChangedFn: (date) {
+          tempPrint(date);
+        },
+        name: 'date');
+  }
+
+  Widget _buildButtons(BuildContext context, WidgetRef ref) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            icon: const ApproveIcon(),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const CancelIcon(),
+            onPressed: () => {},
+          ),
+        ],
+      ),
+    );
   }
 }
 
 void addTransactionToDb(WidgetRef ref, Transaction transaction) {
   final repository = ref.read(transactionRepositoryProvider);
   repository.addItem(transaction);
+}
+
+class FormFieldLabel extends StatelessWidget {
+  const FormFieldLabel(this.label, {super.key});
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(2),
+        child: Text(label,
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 18, color: Colors.black)));
+  }
 }
 
 // Transaction getTestTransaction() {
