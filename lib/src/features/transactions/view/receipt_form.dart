582ec17b339ade6483bcc8f5c5a849e307b9d862
Shaheen <AiExpert2025@gmail.com>
2025-01-14 11:26:40 +0300

    convert ReceiptForm to ConsumerStatefulWidget

--- lib/src/features/transactions/view/receipt_form.dart
+++ lib/src/features/transactions/view/receipt_form.dart
@@ -13,54 +13,62 @@ import 'package:tablets/src/features/transactions/controllers/form_data_containe
 import 'package:tablets/src/features/transactions/model/transaction.dart';
 import 'package:tablets/src/features/transactions/repository/transaction_repository_provider.dart';
 
-class ReceiptForm extends ConsumerWidget {
+class ReceiptForm extends ConsumerStatefulWidget {
   const ReceiptForm({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ReceiptForm> createState() => _ReceiptFormState();
+}
+
+class _ReceiptFormState extends ConsumerState<ReceiptForm> {
+  double total = 0.0; // Initialize total to 0
+
+  @override
+  Widget build(BuildContext context) {
     final formDataNotifier = ref.read(formDataContainerProvider.notifier);
-    final totalTextEditingController = TextEditingController(text: '0');
+    ref.watch(formDataContainerProvider);
+
     return MainFrame(
-        child: Center(
-            child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                width: 400,
-                child: Column(
-                  children: [
-                    VerticalGap.xl,
-                    _buildScreenTitle(context, ref),
-                    VerticalGap.xl,
-                    _buildNameSelection(context, ref, formDataNotifier),
-                    VerticalGap.xl,
-                    _buildReceiptNumber(context, ref, formDataNotifier),
-                    VerticalGap.xl,
-                    _buildDate(context, ref, formDataNotifier),
-                    VerticalGap.xl,
-                    _buildReceivedAmount(
-                        context, ref, formDataNotifier, totalTextEditingController),
-                    VerticalGap.xl,
-                    _buildDiscountAmount(
-                        context, ref, formDataNotifier, totalTextEditingController),
-                    VerticalGap.xl,
-                    _buildReceiptTotalAmount(
-                        context, ref, formDataNotifier, totalTextEditingController),
-                    const Spacer(),
-                    _buildButtons(context, ref, formDataNotifier),
-                  ],
-                ))));
+      child: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          width: 400,
+          child: Column(
+            children: [
+              VerticalGap.xl,
+              _buildScreenTitle(context),
+              VerticalGap.xl,
+              _buildNameSelection(context, formDataNotifier),
+              VerticalGap.xl,
+              _buildReceiptNumber(context, formDataNotifier),
+              VerticalGap.xl,
+              _buildDate(context, formDataNotifier),
+              VerticalGap.xl,
+              _buildReceivedAmount(context, formDataNotifier),
+              VerticalGap.xl,
+              _buildDiscountAmount(context, formDataNotifier),
+              VerticalGap.xl,
+              _buildReceiptTotalAmount(context),
+              const Spacer(),
+              _buildButtons(context, formDataNotifier),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  Widget _buildScreenTitle(BuildContext context, WidgetRef ref) {
+  Widget _buildScreenTitle(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'قبض زبون',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
-        ));
+      padding: const EdgeInsets.all(10),
+      child: const Text(
+        'قبض زبون',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red),
+      ),
+    );
   }
 
-  Widget _buildNameSelection(
-      BuildContext context, WidgetRef ref, MapStateNotifier formDataNotifier) {
+  Widget _buildNameSelection(BuildContext context, MapStateNotifier formDataNotifier) {
     final salesmanCustomersDb = ref.read(salesmanCustomerDbCacheProvider.notifier);
 
     return Row(
@@ -80,97 +88,100 @@ class ReceiptForm extends ConsumerWidget {
     );
   }
 
-  Widget _buildReceivedAmount(BuildContext context, WidgetRef ref,
-      MapStateNotifier formDataNotifier, TextEditingController totalEditingController) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const FormFieldLabel('التسديد'),
-      HorizontalGap.xl,
-      Expanded(
-        child: FormInputField(
-          onChangedFn: (value) {
-            formDataNotifier.addProperty('subTotalAmount', value);
-            final discount = formDataNotifier.data['discount'] ?? 0;
-            final total = value + discount;
-            formDataNotifier.addProperty('totalAmount', total);
-            totalEditingController.text = total;
-          },
-          dataType: FieldDataType.num,
-          name: 'subtotal',
+  Widget _buildReceivedAmount(BuildContext context, MapStateNotifier formDataNotifier) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FormFieldLabel('التسديد'),
+        HorizontalGap.xl,
+        Expanded(
+          child: FormInputField(
+            onChangedFn: (value) {
+              formDataNotifier.addProperty('subTotalAmount', value);
+              final discount = formDataNotifier.data['discount'] ?? 0;
+              total = value + discount; // Update total
+              formDataNotifier.addProperty('totalAmount', total);
+            },
+            dataType: FieldDataType.num,
+            name: 'subtotal',
+          ),
         ),
-      )
-    ]);
+      ],
+    );
   }
 
-  Widget _buildDiscountAmount(BuildContext context, WidgetRef ref,
-      MapStateNotifier formDataNotifier, TextEditingController totalEditingController) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const FormFieldLabel('الخصم'),
-      HorizontalGap.xl,
-      Expanded(
-        child: FormInputField(
-          onChangedFn: (value) {
-            formDataNotifier.addProperty('discount', value);
-            final subtotal = formDataNotifier.data['subTotalAmount'] ?? 0;
-            final total = value + subtotal;
-            formDataNotifier.addProperty('totalAmount', total);
-            totalEditingController.text = total;
-          },
-          dataType: FieldDataType.num,
-          name: 'discount',
+  Widget _buildDiscountAmount(BuildContext context, MapStateNotifier formDataNotifier) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FormFieldLabel('الخصم'),
+        HorizontalGap.xl,
+        Expanded(
+          child: FormInputField(
+            onChangedFn: (value) {
+              formDataNotifier.addProperty('discount', value);
+              final subtotal = formDataNotifier.data['subTotalAmount'] ?? 0;
+              total = value + subtotal; // Update total
+              formDataNotifier.addProperty('totalAmount', total);
+            },
+            dataType: FieldDataType.num,
+            name: 'discount',
+          ),
         ),
-      )
-    ]);
+      ],
+    );
   }
 
-  Widget _buildReceiptNumber(
-      BuildContext context, WidgetRef ref, MapStateNotifier formDataNotifier) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const FormFieldLabel('رقم الوصل'),
-      HorizontalGap.xl,
-      Expanded(
-        child: FormInputField(
-          onChangedFn: (value) {
-            formDataNotifier.addProperty('number', value);
-          },
-          dataType: FieldDataType.num,
-          name: 'number',
+  Widget _buildReceiptNumber(BuildContext context, MapStateNotifier formDataNotifier) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FormFieldLabel('رقم الوصل'),
+        HorizontalGap.xl,
+        Expanded(
+          child: FormInputField(
+            onChangedFn: (value) {
+              formDataNotifier.addProperty('number', value);
+            },
+            dataType: FieldDataType.num,
+            name: 'number',
+          ),
         ),
-      )
-    ]);
+      ],
+    );
   }
 
-  Widget _buildReceiptTotalAmount(BuildContext context, WidgetRef ref,
-      MapStateNotifier formDataNotifier, TextEditingController totalEditingController) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const FormFieldLabel('المجموع'),
-      HorizontalGap.xl,
-      Expanded(
-        child: FormInputField(
-          controller: totalEditingController,
-          onChangedFn: (value) {},
-          dataType: FieldDataType.num,
-          isReadOnly: true,
-          name: 'total',
-        ),
-      )
-    ]);
+  Widget _buildReceiptTotalAmount(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FormFieldLabel('المجموع'),
+        HorizontalGap.xl,
+        Text(total.toString()),
+      ],
+    );
   }
 
-  Widget _buildDate(BuildContext context, WidgetRef ref, MapStateNotifier formDataNotifier) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      const FormFieldLabel('التاريخ'),
-      HorizontalGap.xl,
-      Expanded(
+  Widget _buildDate(BuildContext context, MapStateNotifier formDataNotifier) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FormFieldLabel('التاريخ'),
+        HorizontalGap.xl,
+        Expanded(
           child: FormDatePickerField(
-              initialValue: DateTime.now(),
-              onChangedFn: (date) {
-                formDataNotifier.addProperty('date', date);
-              },
-              name: 'date'))
-    ]);
+            initialValue: DateTime.now(),
+            onChangedFn: (date) {
+              formDataNotifier.addProperty('date', date);
+            },
+            name: 'date',
+          ),
+        ),
+      ],
+    );
   }
 
-  Widget _buildButtons(BuildContext context, WidgetRef ref, MapStateNotifier formDataNotifier) {
+  Widget _buildButtons(BuildContext context, MapStateNotifier formDataNotifier) {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
@@ -204,24 +215,13 @@ class FormFieldLabel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 90,
-        padding: const EdgeInsets.all(2),
-        child: Text(label,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 18, color: Colors.black)));
+      width: 90,
+      padding: const EdgeInsets.all(2),
+      child: Text(
+        label,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 18, color: Colors.black),
+      ),
+    );
   }
 }
-
-// Transaction getTestTransaction() {
-//   return Transaction(
-//       dbRef: 'sdfsd',
-//       name: 'test pending transaction',
-//       imageUrls: ['xfdsfsdf'],
-//       number: 3243456,
-//       date: DateTime.now(),
-//       currency: 'sdfasdf',
-//       transactionType: 'sfsdfs',
-//       totalAmount: 34534534,
-//       transactionTotalProfit: 354,
-//       isPrinted: false);
-// }
