7d9757074ddd34e2f2de57a8e79bd2d0088a8deb
Shaheen <AiExpert2025@gmail.com>
2025-01-27 10:09:12 +0300

    ...

--- lib/src/common/functions/utils.dart
+++ lib/src/common/functions/utils.dart
@@ -37,4 +37,4 @@ String doubleToStringWithComma(dynamic value,
   return formattedWholePart + decimalPart;
 }
 
-String formatDate(DateTime date) => DateFormat('yyyy/MM/dd', 'en_US').format(date);
+String formatDate(DateTime date) => DateFormat('dd/MM/yyyy', 'en_US').format(date);
